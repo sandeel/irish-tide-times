2e31864e6feb3844071a7e39f6d5ea41827f1f68
Daniel <sandeelstudios@gmail.com>
2014-11-15 12:16:57 +0000

    Twilio url now responds to location sent in sms with tides times. Forces fetch of tide times if they haven't been fetched yet. Responds with error and list of location codes if invalid location code submitted

--- app/tides/views.py
+++ app/tides/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
 import twilio.twiml
+import tides.models
+from .models import Tide
+from datetime import date
 
 def receive_sms(request):
+    
+    location = request.GET.get('Body', '')
 
-    """Respond and greet the caller by name."""
- 
-    message = "Monkey, thanks for the message!"
- 
+    sorted_locations = sorted(Tide.locations)
+    if location in sorted_locations:
+        results =  Tide.objects.all().filter(location=location,date=date.today())
+        if not results:
+            tides.models.get_tides()
+            results =  Tide.objects.all().filter(location=location,date=date.today())
+
+        tide = results[0]
+
+        message = "Tides for %s at location %s\nFirst Low: %s\nFirst High: %s\nSecond Low: %s\nSecond High: %s" % (
+            date.today(), location, tide.first_low, tide.first_high, tide.second_low, tide.second_high)
+    else:
+        message = "Sorry, can't find tides for that location code. Available location codes are:\n"
+        for location in sorted_locations:
+            message += "%s,\n" % location
+
     resp = twilio.twiml.Response()
-    resp.message(request.GET.get('Body', ''))
+    resp.message(message)
 
     return HttpResponse(resp, content_type='text/xml')
